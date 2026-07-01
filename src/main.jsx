f5ad4eefab05390890f9e1d794efbdf46e276fc3
Nick <nickkoutsoulidakis@gmail.com>
2026-07-01 16:12:47 -0400

    Add dist folder for GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store.js'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
@@ -11,3 +13,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </Provider>
   </React.StrictMode>,
 )
+
